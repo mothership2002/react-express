e1856f8a3106ddfe7fecd59542b45270e1828685
Coding ARBA <arbagamez@gmail.com>
2022-12-26 16:15:39 +0900

    express method 예제

--- express/routes/index.js
+++ express/routes/index.js
@@ -8,6 +8,32 @@ router.get('/', function(req, res, next) {
 
 router.get("/index", function(req, res, next) {
   res.render("hello_world", { title : 'test'});
-})
+});
+
+router.get('/getTestJson', (req, res, next) => {
+  const result = [
+    {
+      no: 0,
+      title: '기모링',
+      userNickname: '호고고',
+      regDateTime: '2022.12.26 10:10:10'
+    },
+    {
+      no: 1,
+      title: '기모링1',
+      userNickname: '호고고1',
+      regDateTime: '2022.12.26 10:10:10'
+    },
+    {
+      no: 2,
+      title: '기모링2',
+      userNickname: '호고고2',
+      regDateTime: '2022.12.26 10:10:10'
+    },
+  ];
+  
+  res.json(result);
+  // res.send(result);
+});
 
 module.exports = router;
